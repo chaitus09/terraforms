9b1ff3696345407585b02a1d031caec69efe6345
chaitu <chaitus.ch09@gmail.com>
2024-03-14 22:50:24 -0700

    create ec2 instances

---
+++ expense/ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids=["sg-0c06371b05b6070a7"]
+    tags = {
+    Name="frontend"
+  }
+
+}
+resource "aws_instance" "backend" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids=["sg-0c06371b05b6070a7"]
+  tags = {
+    Name="backend"
+  }
+}
+resource "aws_instance" "mysql" {
+  ami           = "ami-05f020f5935e52dc4"
+  vpc_security_group_ids=["sg-0c06371b05b6070a7"]
+  instance_type = "t3.micro"
+  tags = {
+    Name="mysql"
+  }
+}
