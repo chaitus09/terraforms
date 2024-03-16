317d4533b31aa0d911dc8645d6ffa555fd176861
chaitu <chaitus.ch09@gmail.com>
2024-03-16 15:17:33 -0700

    varibles and printing using list and maps

--- variable/var.tf
+++ variable/var.tf
@@ -8,4 +8,25 @@ variable "x" {
 output "x" {
   value = var.x
 }
+ # list
+variable "y_list"
+{
+  default=[1,2,3]
+}
 
+output "y" {
+  value =var.y_list
+}
+
+variable "z_map"
+{
+  default=
+  {
+    x=10,
+    y=20
+  }
+}
+
+output "z" {
+  value =var.z_map
+}
